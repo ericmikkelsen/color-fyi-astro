d9bb550ed73c9c8d713bd316ecf60b7e0a5ee47f
Eric! <eric@ericmikkelsen.com>
2024-03-11 09:20:17 -0700

    feat: streamlines output of colors

--- src/modules/colorTable.js
+++ src/modules/colorTable.js
@@ -1,26 +1,5 @@
 import Color from "colorjs.io";
 import slugify from '@sindresorhus/slugify';
-const a11yObject = (contrast) => {
-    const text = contrast >= 4.5;
-    const largeText = contrast >= 3;
-    const interactive = contrast >= 3;
-    const wcagAAAText = contrast >= 7;
-    const wcagAAALargeText = contrast >= 4.5;
-    return {
-        AA: {
-            text: text,
-            largeText: largeText,
-            interactive: interactive,
-        },
-        AAA: {
-            text: wcagAAAText,
-            largeText: wcagAAALargeText
-        },
-        text: {
-            large: 'Large text is text has a font size of at last 24 pixels or a font size of 19 pixels and a font weight of bold(700).'
-        }
-    }
-}
 const contrastText = (contrast) => {
     const good = contrast >= 4.5;
     const goodForLargeText = contrast >= 3;
@@ -30,7 +9,7 @@ const contrastText = (contrast) => {
     let text = `${iconX} is not accessible`
     if(good) {
         text = `<svg class="icon-yes" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="4" fill="none">
-        <path id="a" d="m19.9 5.8-10 11-6-5"/>
+        <path d="m19.9 5.8-10 11-6-5"/>
     </svg> is accessible`
     } else if (goodForLargeText) {
         text = `<svg class="icon-large" xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke-width="2"
@@ -65,12 +44,14 @@ const colorList = (colors, headingLevel = 2, prefix = 'color-list') => {
     colorObjects.forEach((color1,index,) => {
         if(color1) {
             colorObjects.forEach((color2) =>{
-                if(! colorObjects[index].colors) {
-                    colorObjects[index].colors = [];
-                }
-                if(color2) {
-                    const contrast = color1.data.contrast(color2.data,"WCAG21")
-                    colorObjects[index].colors.push({...color2, contrast: contrast})
+                if(color1 !== color2){
+                    if(! colorObjects[index].colors) {
+                        colorObjects[index].colors = [];
+                    }
+                    if(color2) {
+                        const contrast = color1.data.contrast(color2.data,"WCAG21")
+                        colorObjects[index].colors.push({...color2, contrast: contrast})
+                    }
                 }
             })
         }
@@ -81,28 +62,44 @@ const colorList = (colors, headingLevel = 2, prefix = 'color-list') => {
             ${text}
         </h${h}>`;
     }
-    return `<div class="${prefix}">
-        ${colorObjects.map((color,index)=>{
-            return `${heading(index+1 + '. ' + color.name.trim())}
-                ${color.colors.map(col => {
-                    return `<p>        
-                            <span class="${prefix}-color-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16">
-                                <circle fill="${color.data}" cx="8" cy="8" r="7"/>
-                                </svg> 
-                                ${color.name}
-                            </span>
-                            <span>with</span>
-                            <span class="${prefix}-color-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16">
-                                    <circle fill="${col.data}" cx="8" cy="8" r="7"/>
-                                </svg> ${col.name}
-                            </span>
-                            <span class="${prefix}-info">${contrastText(col.contrast)}</span>
-                    </p>`;
-                }).join('')}
-            `;
-        }).join('')}
-    </div>`;
+    const colorSwatch = (name,color,number) => {
+        return `<span class="${prefix}-color-${number}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16">
+            <circle fill="${color}" cx="8" cy="8" r="7"/>
+            </svg> 
+            ${name}
+        </span>`
+    }
+    
+    if(colorObjects.length === 2 ){
+        const [color1,color2] = colorObjects;
+        console.log(color1.colors[0].contrast)
+        return `<div class="${prefix}">
+        ${heading('Comparison')}
+        <p>        
+            ${colorSwatch( color1.name, color1.data, 1 )}
+            <span>with</span>
+            ${colorSwatch( color2.name, color2.data, 2 )}
+            <span class="${prefix}-info">${contrastText(color1.colors[0].contrast)}</span>
+        </p>
+        </div>`;
+    }
+    if(colorObjects.length > 2 ) {
+        return `<div class="${prefix}">
+            ${colorObjects.map((color,index)=>{
+                return `${heading(index+1 + '. ' + color.name.trim())}
+                    ${color.colors.map(col => {
+                        return `<p>        
+                                ${colorSwatch(color.name,color.data,1)}
+                                <span>with</span>
+                                ${colorSwatch(col.name,col.data,2)}
+                                <span class="${prefix}-info">${contrastText(col.contrast)}</span>
+                        </p>`;
+                    }).join('')}
+                `;
+            }).join('')}
+        </div>`;
+    }
+    return '';
 }
 export default colorList
