2ed35f76c272aa261fdd6f5ba20400c546beb8b7
Eric! <eric@ericmikkelsen.com>
2024-03-18 23:11:16 -0700

    feat: updates description

--- src/modules/compare2ColorsText.js
+++ src/modules/compare2ColorsText.js
@@ -6,15 +6,13 @@ export default (color1, color2) => {
         const onlyLargeText =  4.5 > contrast && contrast >= 3;
         const good = contrast >= 4.5;
         let text = `${colors[0].name} with ${colors[1].name} is not accessible`;
-        let description = `${colors[0].name} and ${colors[1].name} have a contrast of ${contrast}.`
-        const largeText = 'Large text is considered at least 24px or bold(700) and at least 19px.'
+        let description = `${colors[0].name} and ${colors[1].name} have a contrast ratio of ${contrast} to 1.
+            In order to be accessible, text and background colors need at least a contrast of at least 4.5 to 1 to or 3 to 1 for large text.`
+            +`Large text is considered at least 24px or bold(700) and at least 19px.`
         if( onlyLargeText ) {
             text = `${colors[0].name} with ${colors[1].name} is only accessible with large text`;
-            description = `${colors[0].name} and ${colors[1].name} have a contrast of ${contrast}. ${largeText}`
         } else if ( good ) {
             text = `${colors[0].name} with ${colors[1].name} is accessible`;
-        } else {
-            description = description + ` In order to be accessible, text and background colors need at least a contrast of at least 4.5 or 3.0 for large text. ${largeText}`
         }
         return {
             colors: colors,
