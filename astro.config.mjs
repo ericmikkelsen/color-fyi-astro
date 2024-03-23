5efab9b0e72b1c068d0e510d162471314f980e7c
Eric! <eric@ericmikkelsen.com>
2024-03-22 20:33:17 -0700

    feat: trying out vite pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,31 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   adapter: netlify({
-    edgeMiddleware: true,
+    edgeMiddleware: true
   }),
   output: 'server',
   site: 'https://colors.fyi',
+  integrations: [sitemap()],
+  vite: {
+    // https://github.com/shaunchander/astro-pwa-starter/blob/master/astro.config.ts
+		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest,
+				workbox: {
+				  globDirectory: 'dist',
+				  globPatterns: [
+				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+				  ],
+				  // Don't fallback on document based (e.g. `/some-page`) requests
+				  // This removes an errant console.log message from showing up.
+				  navigateFallback: null,
+				},
+			})
+		]
+	}
 });
