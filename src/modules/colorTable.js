4a4711b7b976890973c9028caf9c25213b830bf8
Eric! <eric@ericmikkelsen.com>
2024-04-23 23:36:29 -0700

    feat: adds contrast ratio text

--- src/modules/colorTable.js
+++ src/modules/colorTable.js
@@ -3,22 +3,23 @@ import getColors from './getColors.js'
 import contrastGrade from './contrastGrade.js';
 const contrastText = (contrast) => {
     const good = contrast >= 4.5;
+    const readableContrast = parseFloat(contrast.toFixed(1)).toString()
     const goodForLargeText = contrast >= 3;
     const iconX = `<svg class="icon-no" xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" stroke-width="4">
     <path d="m6 6 12 12M6 18 18 6"/>
   </svg>`
-    let text = `${iconX} is not accessible`
+    let text = `${iconX} is not accessible with a contrast of ${readableContrast}`
     if(good) {
         text = `<svg class="icon-yes" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="4" fill="none">
         <path d="m19.9 5.8-10 11-6-5"/>
-    </svg> is accessible`
+    </svg> is accessible with a contrast of ${readableContrast}`
     } else if (goodForLargeText) {
         text = `<svg class="icon-large" xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke-width="2"
         style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round"    
             >
          <path d="M19.5 18.7h-15l7.5-13Z"/>
          <path d="M12 10.9v2.4zm0 5v.1z"/>
-       </svg> is only accessible for large text`
+       </svg> is only accessible for large text with a contrast of ${readableContrast}`
     }
     return text
 }
@@ -32,8 +33,8 @@ const colorList = (colors, headingLevel = 2, prefix = 'color-list') => {
 
     const colorObjects = getColors(colors);
 
-    const heading = (text,h = headingLevel) => {
-        const id = slugify(text)
+    const heading = (text,h = headingLevel, id = false) => {
+        id = id || slugify(text)
         return `<h${h} id="${id}">
             ${text}
         </h${h}>`;
@@ -43,7 +44,7 @@ const colorList = (colors, headingLevel = 2, prefix = 'color-list') => {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16">
             <circle fill="${color}" cx="8" cy="8" r="7"/>
             </svg> 
-            ${name}
+            ${name} ${number === 1 ? ' with ' : ''}
         </span>`
     }
     
@@ -65,12 +66,11 @@ const colorList = (colors, headingLevel = 2, prefix = 'color-list') => {
             ${colorObjects.map((color,index)=>{
                 const isLight = color.data.contrast('black','wcag21') < 4.5;
                 return `<div class="color-list__color ${isLight ? 'color-list__color--light' : 'color-list__color--dark'}" style="--color:${color.data}">
-                    ${heading(index+1 + `. ` + color.name.trim())}
+                ${heading(index+1 + `. ` + color.name.trim(), headingLevel + 1,  slugify(color.name.trim()) )}
                 ${color.colors.map(col => {
                         const grade = contrastGrade(col.contrast);
                         return `<p class="color-list__item color-list__item--${grade}">        
                                 ${colorSwatch(color.name,color.data,1)}
-                                <span>with</span>
                                 ${colorSwatch(col.name,col.data,2)}
                                 <span class="${prefix}-info">${contrastText(col.contrast)}</span>
                         </p>`;
